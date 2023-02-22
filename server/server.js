c022d4310fdebb36019ce34f73765e7bf83bd94e
Kairos22 <alandiko84@gmail.com>
2023-02-22 16:34:25 +0100

    Initialized Express server.

--- server/server.js
+++ server/server.js
@@ -1,1 +1,13 @@
-console.log("hello");
+const express = require("express");
+require("dotenv").config();
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+
+const port = process.env.PORT || 4001;
+app.listen(port, () => console.log("Server is up and running at port", port));
