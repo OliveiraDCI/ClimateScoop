485841d0f3827b4ce527fa6de247f39afb56f6fb
OliveiraDCI <oliveira.sein.dci@gmail.com>
2023-02-27 17:23:21 +0100

    Downgraded React version for compatibility reason with Google API login packages.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,27 @@
-import { useState } from "react";
+import LoginButton from "./components/login";
+import LogoutButton from "./components/logout";
+import { gapi } from "gapi-script";
+import { useState, useEffect } from "react";
 import axios from "axios";
+
+const clientId = clientId;
 
 function App() {
   const [data, setData] = useState({
     email: "",
     password: "",
   });
+
+  useEffect(() => {
+    function start() {
+      gapi.client.init({
+        clientId: clientId,
+        scope: "",
+      });
+    }
+
+    gapi.load("client:auth2", start);
+  }, []);
 
   const handleRegister = async (e) => {
     e.preventDefault();
@@ -31,23 +47,26 @@ function App() {
           <input
             value={data.value}
             onChange={(e) => setData({ ...data, email: e.target.value })}
-            id='email'
-            name='email'
-            type='text'
+            id="email"
+            name="email"
+            type="text"
           />
         </section>
         <section>
           <label>Password</label>
           <input
-            id='password'
+            id="password"
             value={data.password}
             onChange={(e) => setData({ ...data, password: e.target.value })}
-            name='password'
-            type='password'
+            name="password"
+            type="password"
           />
         </section>
         <button onClick={(e) => handleRegister(e)}>Register</button>
+        <LoginButton />
+
         <button onClick={(e) => handleLogin(e)}>Login</button>
+        <LogoutButton />
       </form>
     </>
   );
