c2765bc46319d24b3458a9502c3595580147d80d
OliveiraDCI <oliveira.sein.dci@gmail.com>
2023-03-01 12:25:38 +0100

    User model adjusted to consume Google user profile data.

--- server/models/User.js
+++ server/models/User.js
@@ -3,11 +3,10 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-  email: { type: String, required: true, unique: true },
-  password: {
-    type: String,
-    required: true,
-  },
+  email: { type: String },
+  firstName: { type: String },
+  lastName: { type: String },
+  picture: { type: String },
 });
 
 module.exports = mongoose.model("User", userSchema);
