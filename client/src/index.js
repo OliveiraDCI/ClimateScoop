eb3ed4caf0ec5aa9ae6a7f5adcd189b876f67f9a
OliveiraDCI <oliveira.sein.dci@gmail.com>
2023-03-13 16:03:28 +0100

    Created Routes and organized components.

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./styles/reset.css";
+import "./styles/main.scss";
+import ContextProvider from "./context/ContextProvider";
+import Header from "./layout/Header";
+import App from "./pages/App";
+import Articles from "./pages/Articles";
+import MyPage from "./pages/MyPage";
+import NotFound from "./pages/NotFound";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <ContextProvider>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/articles" element={<Articles />} />
+        <Route path="/myPage" element={<MyPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  </ContextProvider>,
+  document.getElementById("root")
+);
