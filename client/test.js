9377477c6c9544922ad48d31f66cdd479f3edc4a
OliveiraDCI <oliveira.sein.dci@gmail.com>
2023-03-13 16:01:06 +0100

    Installed sass and react-router-dom packages.

--- client/test.js
+++ client/test.js
@@ -5,7 +5,7 @@ console.log("");
 
 async function createFetch() {
   let response = await axios.get(
-    "https://global-warming.org/api/ocean-warming-api"
+    "https://newsdata.io/api/1/news?apikey=pub_180089098407af05742bc64588e14bd077ab6&category=environment&language=en"
   );
   console.log("response", response.data);
   return response;
