5199340cec4e3b3c0ffa46dbaca007b8a55bf729
Kairos22 <alandiko84@gmail.com>
2023-03-23 17:09:54 +0100

    Created and styled Hero section components

--- client/src/components/homePage/Hero.js
+++ client/src/components/homePage/Hero.js
@@ -1,7 +1,40 @@
 import "./hero.scss";
 
-function Hero() {
-  return <div className="hero">Hello from Hero</div>;
+function HomePage() {
+  return (
+    <div className="home-page">
+      <HeroSection />
+      <DashboardSection />
+    </div>
+  );
 }
 
-export default Hero;
+function HeroSection() {
+  return (
+    <div class="bg-image">
+      <section className="hero-section">
+        <h1>Welcome to ClimateScoop</h1>
+        <p>
+          Explore new articles about Climate Change & create your owns if you'd
+          like !
+        </p>
+        <button>Articles</button>
+      </section>
+    </div>
+  );
+}
+
+function DashboardSection() {
+  return (
+    <section className="dashboard-section">
+      <h2>Dashboard</h2>
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+      </ul>
+    </section>
+  );
+}
+
+export default HomePage;
